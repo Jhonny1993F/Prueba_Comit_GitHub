66f215dc58d1ebae73918ade0bab9cc4934f4381
Jhonny Montero <jhonnymontero88@gmail.com>
2023-04-24 13:22:49 -0500

    Agregar archivos de proyecto.

---
+++ Models/Comit.cs
@@ -0,0 +1,11 @@
+﻿namespace Prueba_Comit_GitHub.Models
+{
+    public class Comit
+    {
+        public int Id { get; set; }
+        public DateTime FechaComit { get; set; }
+        public string? Descripcion { get; set; }
+        public bool Funciono { get; set; }
+        public double Calificacion { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prueba_Comit_GitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
